005e94fb4f0b29d038347373da64569aeedf7dbb
Cristian Sánchez Salas <cristian.sanchez.salas@gmail.com>
2018-03-29 04:48:26 -0300

    Add files via upload

---
+++ setear_sitio.sql
@@ -0,0 +1,4 @@
+UPDATE `wwwwebem_wp9`.`wp_options` SET `option_value`='http://academiamarcial.cl' WHERE `option_id`='1';
+UPDATE `wwwwebem_wp9`.`wp_options` SET `option_value`='Academia Marcial' WHERE `option_id`='2';
+UPDATE `wwwwebem_wp9`.`wp_options` SET `option_value`='Directorios de academias de artes marciales en Chile' WHERE `option_id`='3';
+UPDATE `wwwwebem_wp9`.`wp_options` SET `option_value`='http://academiamarcial.cl' WHERE `option_id`='37';
